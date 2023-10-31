7d480961453366cc2f781cbb810fb1869b048110
nguyen tuan <nguyenanhtuan23122000@gmail.com>
2023-10-31 22:18:01 +0700

    commit 2

---
+++ pages/action_2.tsx
@@ -0,0 +1,5 @@
+export default function Action1 () {
+    return <div className="bg-red-100  h-screen w-full text-center">
+        <p className="text-2xl font-medium text-pink-300">I love you</p>
+    </div>
+}
