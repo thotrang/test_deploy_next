3a6c5f510d8778f5530dbe21848e2789be04187f
nguyen tuan <nguyenanhtuan23122000@gmail.com>
2023-11-15 22:24:24 +0700

    fb chat

--- module/fb-chat/FbChat.tsx
+++ module/fb-chat/FbChat.tsx
@@ -1,36 +1,39 @@
-/* eslint-disable @next/next/inline-script-id */
-import Script from "next/script";
 import { useEffect } from "react";
-export default function FBChat() {
-  useEffect(()=>{
 
-  },[])
+const FacebookChat = () => {
+  const setupFB = () => {
+    const chatbox = document.getElementById("fb-customer-chat");
+    chatbox?.setAttribute("page_id", "177959832056731");
+    chatbox?.setAttribute("attribution", "biz_inbox");
+    const config: any = window;
+
+    config.fbAsyncInit = function () {
+      config.FB.init({
+        xfbml: true,
+        version: "v18.0",
+      });
+    };
+
+    (function (d, s, id) {
+      let js: any;
+      let fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s);
+      js.id = id;
+      js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+      fjs.parentNode?.insertBefore(js, fjs);
+    })(document, "script", "facebook-jssdk");
+  };
+  useEffect(() => {
+    setupFB()
+  }, []);
+
   return (
     <div>
       <div id="fb-root"></div>
       <div id="fb-customer-chat" className="fb-customerchat"></div>
-      <Script strategy="lazyOnload">
-        {`
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "112121811861035");
-      chatbox.setAttribute("attribution", "biz_inbox");
-
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v15.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    `}
-      </Script>
     </div>
   );
-}
+};
+
+export default FacebookChat;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import FBChat from '@/module/fb-chat/FbChat';
-import dynamic from 'next/dynamic';
-
 export default function Home() {
   return <div className="h-screen w-screen">
     heheheheh
