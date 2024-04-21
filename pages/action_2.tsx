de12222cb551c208b7491b83d4fe94747958bba4
Thai Bui Minh <=>
2024-04-22 01:14:13 +0700

    continue

--- pages/action_2.tsx
+++ pages/action_2.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import anime from "animejs";
 import Image from "next/image";
 export default function Action1() {
@@ -47,9 +47,13 @@ export default function Action1() {
     line-height: 1em;
   }
   `;
+  const audioRef = useRef<HTMLAudioElement>(null);
   return (
-    <div className=" h-screen w-screen text-7xl leading-[80px] font-medium  relative">
-      <div className="absolute h-screen w-screen p-24 bg-transparent flex justify-center items-center text-center text-pink-200">
+    <div
+      className=" h-screen w-screen text-7xl max-md:text-3xl leading-[80px] font-medium  relative"
+      onClick={() => audioRef?.current?.play()}
+    >
+      <div className="absolute h-screen w-screen px-3 bg-transparent flex justify-center items-center text-center text-pink-200">
         <style>{styles}</style>
         <div className="ml3">
           Không biết nói gì hơn _ Mong em sang tuổi mới luôn vui vẻ, yêu đời,
@@ -61,8 +65,20 @@ export default function Action1() {
         <img
           src="https://gifsec.com/wp-content/uploads/2022/09/heart-gif-10.gif"
           alt=""
-          className="h-full w-full"
+          className="h-full w-full object-cover"
         />
+      </div>
+      <div className="hidden">
+        <audio controls ref={audioRef} autoPlay loop>
+          <source
+            src="/Nhac-Chuong-Nothings-Gonna-Change-My-Love-for-You-Dave-Moffatt.mp3"
+            type="audio/ogg"
+          />
+          <source
+            src="/Nhac-Chuong-Nothings-Gonna-Change-My-Love-for-You-Dave-Moffatt.mp3"
+            type="audio/mpeg"
+          />
+        </audio>
       </div>
     </div>
   );
